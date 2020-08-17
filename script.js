b03aeadf9166ebcda6b409365119431b853351f2
Mahin <mahinyayaa@gmail.com>
2020-08-17 21:45:35 +0600

    some variable name changed

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-const inputSong = () => {
+const searchMusic = () => {
     document.getElementById('allResult').innerHTML = '';
     document.getElementById('lyrics').innerHTML = '';
-    const songName =  document.getElementById('songName').value;
-    fetch(`https://api.lyrics.ovh/suggest/${songName}`)
+    const inputSong =  document.getElementById('inputSong').value;
+    fetch(`https://api.lyrics.ovh/suggest/${inputSong}`)
     .then(res => res.json())
     .then(data => {   
         loadData = data;                              
         for (let i = 0; i < 9; i++) {
-            const title = data.data[i].title;
-            const artist = data.data[i].artist.name;
+            const songTitle = data.data[i].title;
+            const artistName = data.data[i].artist.name;
             document.getElementById('allResult').innerHTML +=  `<div class="single-result row align-items-center  my-3 p-3">
                                                                     <div class="col-md-9">
-                                                                        <h3 class="lyrics-name">${title}</h3>
-                                                                        <p class="author lead">Album by <span id="artist1">${artist}</span></p>
+                                                                        <h3 class="lyrics-name">${songTitle}</h3>
+                                                                        <p class="author lead">Album by <span id="artist1">${artistName}</span></p>
                                                                     </div>
-                                                                    <button onclick="searchLyrics(${i})" class="btn btn-success">Get Lyrics</button>
+                                                                    <button onclick="getLyrics(${i})" class="btn btn-success">Get Lyrics</button>
                                                                 </div>`                                               
         } 
     })
 }
 
-const searchLyrics = (index) => {
-        const song = loadData.data[index].title;
-        const artist = loadData.data[index].artist.name;
-        fetch(`https://api.lyrics.ovh/v1/${artist}/${song}`)
+const getLyrics = (index) => {
+        const songTitle = loadData.data[index].title;
+        const artistName = loadData.data[index].artist.name;
+        fetch(`https://api.lyrics.ovh/v1/${artistName}/${songTitle}`)
         .then(res => res.json())
         .then(data => {
             let lyrics = data.lyrics;
@@ -31,8 +31,8 @@ const searchLyrics = (index) => {
                 lyrics = `Song Lyrics Not Found. Try for another song`
             }
                 document.getElementById('lyrics').innerHTML = `<h1 class="text-info mb-3">Song Lyrics</h1>
-                                                                <h4 class="text-success">Song name: ${song}</h4>
-                                                                <h4 class="text-success">Artist name: ${artist}</h4>
+                                                                <h4 class="text-success">Song name: ${songTitle}</h4>
+                                                                <h4 class="text-success">Artist name: ${artistName}</h4>
                                                                 <pre class="my-4 text-white">${lyrics}</pre>`
         }) 
 }
