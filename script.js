4ec771a24d6057ba20ad092b231b3173a9175d6d
Mahin <mahinyayaa@gmail.com>
2020-08-17 21:33:39 +0600

    hard rock done

--- script.js
+++ script.js
@@ -9,14 +9,13 @@ const inputSong = () => {
         for (let i = 0; i < 9; i++) {
             const title = data.data[i].title;
             const artist = data.data[i].artist.name;
-            document.getElementById('allResult').innerHTML += `<div class="single-result row align-items-center  my-3 p-3">
+            document.getElementById('allResult').innerHTML +=  `<div class="single-result row align-items-center  my-3 p-3">
                                                                     <div class="col-md-9">
-                                                                    <h3 class="lyrics-name">${title}</h3>
-                                                                    <p class="author lead">Album by <span id="artist1">${artist}</span></p>
-                                                                </div>
+                                                                        <h3 class="lyrics-name">${title}</h3>
+                                                                        <p class="author lead">Album by <span id="artist1">${artist}</span></p>
+                                                                    </div>
                                                                     <button onclick="searchLyrics(${i})" class="btn btn-success">Get Lyrics</button>
-                                                               </div>
-                                                          </div>`;                                                
+                                                                </div>`                                               
         } 
     })
 }
@@ -32,9 +31,9 @@ const searchLyrics = (index) => {
                 lyrics = `Song Lyrics Not Found. Try for another song`
             }
                 document.getElementById('lyrics').innerHTML = `<h1 class="text-info mb-3">Song Lyrics</h1>
-                <h4 class="text-success">Song name: ${song}</h4>
-                <h4 class="text-success">Artist name: ${artist}</h4>
-                <pre class="my-4 text-white">${lyrics}</pre>`
+                                                                <h4 class="text-success">Song name: ${song}</h4>
+                                                                <h4 class="text-success">Artist name: ${artist}</h4>
+                                                                <pre class="my-4 text-white">${lyrics}</pre>`
         }) 
 }
 
